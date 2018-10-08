4e69bd443eae5119a922a2c76ab4a51392a7b18f
Alex Putman <43959556+ppintblank1123@users.noreply.github.com>
2018-10-08 13:05:41 -0600

    Add files via upload

--- 004_MouseDraw/src/mousedraw/MouseDraw.java
+++ 004_MouseDraw/src/mousedraw/MouseDraw.java
@@ -1,13 +1,26 @@
 package mousedraw;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 public class MouseDraw extends JFrame {
+	JMenuBar mainMenuBar= new JMenuBar();
+	JMenu fileMenu = new JMenu("File");
+	JMenuItem newMenuItem= new JMenuItem("New");
+	JMenuItem exitMenuItem = new JMenuItem("Exit");
+	JMenuItem saveMenuItem = new JMenuItem("Save");
+	JMenuItem openMenuItem = new JMenuItem("Open");
 	
 	Graphics2D g2D;
 	
@@ -21,9 +34,49 @@ public class MouseDraw extends JFrame {
 			}
 		});
 		getContentPane().setLayout(new GridBagLayout());
-		
+			
+		setJMenuBar(mainMenuBar);
+		fileMenu.setMnemonic('F');
+		mainMenuBar.add(fileMenu);
+		fileMenu.add(newMenuItem);
+		fileMenu.addSeparator();
+		fileMenu.add(saveMenuItem);
+		fileMenu.addSeparator();
+		fileMenu.add(openMenuItem);
+		fileMenu.addSeparator();
+		fileMenu.add(exitMenuItem);
+		mainMenuBar.setBackground(Color.decode("#AAAAAA"));
+		newMenuItem.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				newMenuItemActionPerformed(e);
+			}
+		});
+		exitMenuItem.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				exitMenuItemActionPerformed(e);
+			}
+		});
 	}// constructor end
 	
+	private void exitMenuItemActionPerformed(ActionEvent e) {
+		int response;
+		response = JOptionPane.showConfirmDialog(null, 
+			"Are you sure you want to exit?",
+			"Exit Program",
+			JOptionPane.YES_NO_OPTION,
+			JOptionPane.QUESTION_MESSAGE);
+		if(response == JOptionPane.NO_OPTION) {
+			return;
+		}
+		else {
+			exitForm(null);
+		}
+	}
+	
+	private void newMenuItemActionPerformed(ActionEvent e) {
+		
+	}
+
 	private void exitForm(WindowEvent e) {
 		g2D.dispose();
 		System.exit(0);
