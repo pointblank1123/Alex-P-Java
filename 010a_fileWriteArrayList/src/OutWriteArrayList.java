a8eb24808187332799a1d1c981b9eeb288fbb76c
Alex Putman <43959556+pointblank1123@users.noreply.github.com>
2018-12-06 11:52:07 -0700

    Add files via upload

---
+++ 010a_fileWriteArrayList/src/OutWriteArrayList.java
@@ -0,0 +1,37 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class OutWriteArrayList {
+	
+ public static void main(String[] args) {
+	
+	 File filename = new File("Friends.txt");
+	 // non standard method
+	 ArrayList aList = new ArrayList();
+	 // Standard method --> ArrayList<String>
+	 String friend = " ";
+	 
+	 while (!friend.isEmpty()) {
+		 friend = JOptionPane.showInputDialog("Enter a friend's name.");
+		 if (!friend.isEmpty()) { //if (!friend.isEmpty()) aList.add(friend); this also works
+			 aList.add(friend);
+		 }
+	 }
+	 try {
+		 FileWriter fw = new FileWriter(filename);
+		 Writer output = new BufferedWriter(fw);
+		 int sz = aList.size();
+		 for (int i=0; i < sz; i++) {
+			 output.write(aList.get(i).toString() + "\n");
+		 }
+	 }
+	 catch (Exception e) {
+		 JOptionPane.showMessageDialog(null, "Cannot create that file");
+	 }
+ }
+}
